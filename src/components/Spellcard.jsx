5b52cfa64919afcb6bc11ddc88834905902ec524
moses-codes <moses.ilagan@gmail.com>
2024-03-19 19:51:39 -0700

    fixed loose text

--- src/components/Spellcard.jsx
+++ src/components/Spellcard.jsx
@@ -51,7 +51,7 @@ export default function SpellCard({ spellInfo, handleDeleteSpell }) {
                             <button onClick={handleDeleteSpell} className="btn btn-outline btn-circle btn-error btn-xs ml-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                             </button>
-                        </div>nbfgt
+                        </div>
                     </div>
                     {show && <div className="transition-all duration-75">
                         <h4 className="text-left mb-3">{level} {school}</h4>
